85f758c4dc88c492e72232bfa488e00434a195eb
xiachangwei <xiachangwei24@mails.ucas.ac.cn>
2026-05-25 14:27:58 +0800

    修复云服务器打开网站白屏问题

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mobile">
       <App />
     </BrowserRouter>
   </StrictMode>
