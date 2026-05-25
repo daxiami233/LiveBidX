a7ac904beb5531b0ead76d31bc6e5761283d748c
xiachangwei <xiachangwei24@mails.ucas.ac.cn>
2026-05-25 14:56:16 +0800

    移除 BrowserRouter 的 basename 属性

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/mobile">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
